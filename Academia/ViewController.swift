65f5c5cafe6af3599f831128bee91d6b97c3d3b9
Rajwinder Singh <irajwinder@users.noreply.github.com>
2023-10-23 08:58:49 -0400

    Initial Commit

---
+++ Academia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Academia
+//
+//  Created by Rajwinder Singh on 10/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
